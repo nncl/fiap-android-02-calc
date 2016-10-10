4fcddf7dc55f29bfceea266d16411d223148b2ea
Caue Bruno de Almeida <RM30917@fiap.com.br>
2016-10-10 20:58:23 -0300

    Initial commit

---
+++ app/src/main/java/com/fiap/mob/calcproject/MainActivity.java
@@ -0,0 +1,31 @@
+package com.fiap.mob.calcproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtNumber1, edtNumber2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // (EditText) => explicit conversion
+        edtNumber1 = (EditText) findViewById(R.id.edtNumber1); // pointing from XML file
+        edtNumber2 = (EditText) findViewById(R.id.edtNumber2);
+    }
+
+    public void sum(View v) {
+        int number1 = Integer.parseInt(edtNumber1.getText().toString());
+        int number2 =  Integer.parseInt(edtNumber2.getText().toString());
+        int total = number1 + number2;
+
+        Toast.makeText(this, getString(R.string.lblTotal) + total, Toast.LENGTH_SHORT).show();
+    }
+
+}
